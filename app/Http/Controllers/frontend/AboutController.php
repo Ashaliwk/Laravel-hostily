0bd7744b6e1d2a1466f0abb8f1d2040b7a67d287
M Ali Weber <127172065+Ashaliwk@users.noreply.github.com>
2026-04-28 01:38:34 -0700

    Updated Laravel project

--- app/Http/Controllers/frontend/AboutController.php
+++ app/Http/Controllers/frontend/AboutController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\frontend;
 
 use Illuminate\Routing\Controller;
+use App\Models\backend\Team;
+
 
 class aboutcontroller extends Controller
 {
     public function index()
     {
-        return view('frontend.about');
+
+        $teams = Team::all();
+        return view('frontend.about', compact('teams'));
     }
 }
--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -123,100 +123,50 @@
 	</div>
 </div>
 <!-- Gallery Area End -->
-<!-- Team Area Start -->
-<div class="team__area section-padding">
-	<div class="container mb-5">
-		<div class="row mb-60">
-			<div class="col-xl-12">
-				<div class="team__area-title">
-					<span class="subtitle__one">Our Exprt Meamber</span>
-					<h2>Expert Our Services man</h2>
-				</div>
-			</div>
-		</div>
-		<div class="row justify-content-center">
-			<div class="col-xl-4 col-lg-4 col-md-6 lg-mb-30">
-				<div class="team__area-item">
-					<div class="team__area-item-image">
-						<img src="assets/img/team/team-1.jpg" alt="">
-						<div class="team__area-item-image-overlay">
-							<div class="team__area-item-image-overlay-social">
+
+<div class="services__team section-padding">
+	<div class="container">
+		<div class="row">
+			@forelse($teams as $team)
+			<div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+				<div class="services__team-item">
+					<div class="services__team-item-image">
+						<img src="{{ asset('uploads/team/'.$team->image) }}" alt="">
+						<div class="services__team-item-image-content">
+							<h5>{{ $team->fullname }}</h5>
+							<span>{{ $team->designation }}</span>
+							<div class="services__team-item-image-content-social">
 								<ul>
-									<li><a href="#"><i class="fab fa-facebook-f"></i></a>
+									@if($team->insta)
+									<li>
+										<a href="{{ $team->insta }}" target="_blank">
+											<i class="fab fa-instagram"></i>
+										</a>
 									</li>
-									<li><a href="#"><i class="fab fa-twitter"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-behance"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-youtube"></i></a>
-									</li>
+									@endif
 								</ul>
-							</div>
-							<div class="team__area-item-image-overlay-content">
-								<h5>Michelle yeoh</h5>
-								<span>Services man</span>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-xl-4 col-lg-4 col-md-6 md-mb-30">
-				<div class="team__area-item">
-					<div class="team__area-item-image">
-						<img src="assets/img/team/team-2.jpg" alt="">
-						<div class="team__area-item-image-overlay">
-							<div class="team__area-item-image-overlay-social">
-								<ul>
-									<li><a href="#"><i class="fab fa-facebook-f"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-twitter"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-behance"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-youtube"></i></a>
-									</li>
-								</ul>
-							</div>
-							<div class="team__area-item-image-overlay-content">
-								<h5>David Fincher</h5>
-								<span>Services man</span>
-							</div>
-						</div>
-					</div>
-				</div>
+
+			@empty
+			<div class="col-12 text-center">
+				<p>No team members found.</p>
 			</div>
-			<div class="col-xl-4 col-lg-4 col-md-6">
-				<div class="team__area-item">
-					<div class="team__area-item-image">
-						<img src="assets/img/team/team-3.jpg" alt="">
-						<div class="team__area-item-image-overlay">
-							<div class="team__area-item-image-overlay-social">
-								<ul>
-									<li><a href="#"><i class="fab fa-facebook-f"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-twitter"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-behance"></i></a>
-									</li>
-									<li><a href="#"><i class="fab fa-youtube"></i></a>
-									</li>
-								</ul>
-							</div>
-							<div class="team__area-item-image-overlay-content">
-								<h5>Michelle Reis</h5>
-								<span>Services man</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			@endforelse
+
 		</div>
 	</div>
+</div>
 
-	<div class="scroll-up">
-		<svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-			<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
-		</svg>
-	</div>
 
-	@endsection
+
+<div class="scroll-up">
+	<svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+		<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+	</svg>
+</div>
+
+@endsection
